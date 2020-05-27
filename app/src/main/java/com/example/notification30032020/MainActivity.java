5c32b1bc4c709a36da2c7f961c6c96e271b4a837
phatdroid94 <phatdroid94@gmail.com>
2020-05-27 20:39:59 +0700

    create ui notificationcompat

--- app/src/main/java/com/example/notification30032020/MainActivity.java
+++ app/src/main/java/com/example/notification30032020/MainActivity.java
@@ -1,14 +1,67 @@
 package com.example.notification30032020;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mBtnOpen, mBtnClose;
+    String CHANEL_ID = "1";
+    String CHANEL_NAME = "chanel_app";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBtnClose = findViewById(R.id.buttonClose);
+        mBtnOpen = findViewById(R.id.buttonOpen);
+
+        // 1 : Tao giao dien notification : NotificationCompat.Builder
+        // 2 : Khi click notification se thuc thi viec gi : PendingIntent
+        // 3 : Tat notification
+        mBtnOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NotificationCompat.Builder builder =
+                        new NotificationCompat.Builder(MainActivity.this, CHANEL_ID)
+                                .setContentTitle("Co thong bao moi")
+                                .setContentText("Co phien moi ban muon cap nhap khong")
+                                .setSmallIcon(R.mipmap.ic_launcher)
+                                .setWhen(System.currentTimeMillis())
+                                .setStyle(
+                                        new NotificationCompat
+                                                .BigPictureStyle()
+                                                .bigPicture(
+                                                        BitmapFactory
+                                                                .decodeResource(
+                                                                getResources(),
+                                                                R.mipmap.ic_launcher))
+
+                                );
+                NotificationManager notificationManager =
+                        (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                    NotificationChannel notificationChannel = new NotificationChannel
+                            (CHANEL_ID , CHANEL_NAME , NotificationManager.IMPORTANCE_DEFAULT );
+                    notificationChannel.enableVibration(true);
+                    notificationManager.createNotificationChannel(notificationChannel);
+                }
+
+                notificationManager.notify(1 ,builder.build());
+
+            }
+        });
+
     }
 }
